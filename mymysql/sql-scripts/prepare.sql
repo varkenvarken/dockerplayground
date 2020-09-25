22bd0eeddccc9d523c1f2d8bfecd12db52571930
varkenvarken <michel@michelanders.nl>
2020-09-25 16:38:06 +0200

    move to more logical project setup

--- mymysql/sql-scripts/prepare.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'dbuser'@'localhost' IDENTIFIED BY 'secret';
-CREATE USER 'dbuser'@'%' IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON log_db.* TO 'dbuser'@'localhost';
-GRANT ALL ON log_db.* TO 'dbuser'@'%';
