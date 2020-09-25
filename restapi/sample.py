ce3f23cf2212de48e4f2d09745b7a658ab039b45
varkenvarken <michel@michelanders.nl>
2020-09-25 11:15:12 +0200

    initial version of Dockerfile

--- restapi/sample.py
+++ restapi/sample.py
@@ -18,23 +18,36 @@ class EmployeeCollectionResource(CollectionResource):
 class EmployeeResource(SingleResource):
     model = Employee
 
-from sqlalchemy import create_engine
-import falcon
-from falcon_autocrud.middleware import Middleware
+if __name__ == '__main__':
+    import argparse
+    from wsgiref.simple_server import make_server
 
-db_engine = create_engine('sqlite:///stuff.db')
+    from sqlalchemy import create_engine
+    import falcon
+    from falcon_autocrud.middleware import Middleware
 
-Base.metadata.create_all(db_engine)
 
-app = falcon.API(
-    middleware=[Middleware()],
-)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--database', '-d', default='sqlite:///stuff.db',
+                        help='Specify alternative directory '
+                        '[default: sqlite:///stuff.db]')
+    parser.add_argument('port', action='store',
+                        default=5555, type=int,
+                        nargs='?',
+                        help='Specify alternate port [default: 5555]')
+    args = parser.parse_args()
+    
+    db_engine = create_engine(args.database)       # 'sqlite:////absolute/path/to/foo.db'
 
-app.add_route('/employees', EmployeeCollectionResource(db_engine))
-app.add_route('/employees/{id}', EmployeeResource(db_engine))
+    Base.metadata.create_all(db_engine)
 
-from wsgiref.simple_server import make_server
+    app = falcon.API(
+        middleware=[Middleware()],
+    )
 
-with make_server('', 5555, app) as httpd:
-    print("Serving on port 5555 ...")
-    httpd.serve_forever()
+    app.add_route('/employees', EmployeeCollectionResource(db_engine))
+    app.add_route('/employees/{id}', EmployeeResource(db_engine))
+
+    with make_server('', args.port, app) as httpd:
+        print(f"Serving on port {args.port} ...")
+        httpd.serve_forever()
