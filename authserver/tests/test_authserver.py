f2eaf74e0cc64d89f4cf5a305133ddadcd9174c6
varkenvarken <michel@michelanders.nl>
2020-11-18 12:27:33 +0100

    added coverage reporting

--- authserver/tests/test_authserver.py
+++ authserver/tests/test_authserver.py
@@ -1,7 +1,13 @@
+import pytest
+
 from authserver import server
 
+@pytest.fixture
+def password():
+    return '12345678'
 
-class TestClass:
 
-    def test_a(self):
-        self.password = server.newpassword('12345678')
+def test_checkpassword(password):
+    pwhash = server.newpassword(password)
+    assert pwhash
+    assert server.checkpassword(password, pwhash)
