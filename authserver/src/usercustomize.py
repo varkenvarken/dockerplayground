6215e6d92136d63f64f6c255680d73cd5c8c36e3
varkenvarken <michel@michelanders.nl>
2020-11-19 14:25:48 +0100

    guard against coverage module not present

--- authserver/src/usercustomize.py
+++ authserver/src/usercustomize.py
@@ -1,5 +1,13 @@
-import coverage
-coverage.process_startup()
-
 import os
-print(os.environ['COVERAGE_PROCESS_START'] )
+
+coverage = "coverage module not imported"
+try:
+    import coverage
+    coverage.process_startup()
+    var = 'COVERAGE_PROCESS_START'
+    cps = os.environ[var] if var in os.environ else f"{var} not defined"
+    if cps == "":
+        cps = f"{var} is empty"
+    coverage = f"coverage imported, {cps}"
+except ModuleNotFoundError:
+    pass
